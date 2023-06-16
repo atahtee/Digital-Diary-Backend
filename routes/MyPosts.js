80a06f08e7dd6abb09ce9866a2038df52c0f41e6
atahtee <atatisam14@gmail.com>
2023-06-16 09:40:03 +0300

    diary post routes implemented

---
+++ routes/MyPosts.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { getAllPosts,
+        getPost,
+        createPost,
+        deletePost,
+        updatePost
+} from "../controllers/PostControllers.js";
+
+const router = express.Router();
+
+router.get('/', getAllPosts);
+router.get('/:id', getPost);
+router.post('/', createPost);
+router.delete('/:id', deletePost);
+router.patch('/:id', updatePost);
+
+export default router;
